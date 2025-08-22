566468ff927137d77b23a31a4f9ec2d03afb896a
Mohamed Sawah <61028956+MohamedSawahZC@users.noreply.github.com>
2025-08-22 19:03:36 +0300

    refactor: dynamically import vite server

--- server/vite.ts
+++ server/vite.ts
@@ -2,7 +2,6 @@ import express, { type Express } from "express";
 import fs from "fs";
 import path from "path";
 import { type Server } from "http";
-import viteConfig from "../vite.config";
 import { nanoid } from "nanoid";
 
 export function log(message: string, source = "express") {
@@ -18,6 +17,7 @@ export function log(message: string, source = "express") {
 
 export async function setupVite(app: Express, server: Server) {
   const { createServer: createViteServer, createLogger } = await import("vite");
+  const { default: viteConfig } = await import("../vite.config");
   const viteLogger = createLogger();
 
   const serverOptions = {
